124dfd3394c7f3d5ffc00dc7659de5e0b87ae992
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2021-12-08 09:04:24 -0300

    avanzando un poquito mas

--- js/index.js
+++ js/index.js
@@ -1,172 +1,28 @@
-/* Cree 2 clases -Clase mesa, que tiene una propiedad monto y un metodo para recargar el 10%
-si es compra financiada- y -Clase Local Comercial, que tiene 2 propiedades, mesasHabilitadas
-y totalRecaudacion, que en principio se encuentra en cero. Este Local comercial tiene un 
-metodo recaudacionDia, que tiene un while, el iterador comienza en 1 representado a la mesa 1,
-y la condicion es mientras que iterador sea menor o igual a mesasHabilitadas, ejecuta el codigo
-que esta adentro, es decir manda un alert diciendo que se va a sumar el total de la mesa 1,
-mesa 2, y asi sucesivamente, hasta el numero que le paso el usuario de mesas habilitadas.
-luego le pregunta el monto de la mesa y si es compra en efectivo o 2 si es compra con tarjeta.
-En este ultimo caso aplica el recargo del 10% llamando a ese metodo. Luego total recaudacion es 
-la suma de totalRecaudacion y el monto de la mesa, i++ va sumando mesa a mesa, hasta que se 
-cumple la condicion y tira un alert diciendo que el total de las mesas del dia es igual a 
-totalRecaudacion y la cantidad de mesas habilitadas.
-Posteriormente, creo una constante bar que es un nuevo objeto LocalComercial y pido que ingrese
-la cantidad de mesas habilitadas, y este llama a la funcion recaudacionDia, iniciando asi
-el simulador. */
-
-// class Mesa {
-//     constructor(id, monto) {
-//         this.id = Number(id);
-//         this.monto = monto;
-//     }
-
-//     /* Sumar recargo del 10% por compra financiada */
-//     sumaRecargoDiez() {
-//         this.monto = Math.round(this.monto * 1.10); 
-//     }
-// }
-
-// class LocalComercial {
-//     constructor() {
-//         this.mesasHabilitadas = [];
-//         this.totalRecaudacion = 0;
-//     }
-
-//     setearMesasHabilitadas() {
-//         alert("Ingrese cantidad de mesas que tiene hoy por numero de mesas y totales");
-//         let cantidadMesas = parseInt(prompt("Ingrese cantidad de mesas"));
-//         for (let i = 1; i <= cantidadMesas; i++) {
-//           let id = parseInt(prompt("Ingrese identificador numerico unico de la mesa"));  
-//           let monto = parseFloat(prompt("Ingrese monto de la mesa"));
-//           let mesa = new Mesa(id, monto);
-//           this.mesasHabilitadas.push(mesa);
-//         }
-//     }
-
-//     recaudacionDia() {
-//        alert("Este metodo devuelve la recaudacion total de las mesas habilitadas");
-//         let sumador = 0;
-//        for(let i = 0; i < this.mesasHabilitadas.length; i++) {
-//            this.totalRecaudacion = this.totalRecaudacion + this.mesasHabilitadas[i].monto;
-//        } 
-//        console.log(this.totalRecaudacion);
-
-//        localStorage.setItem("recaudacion", JSON.stringify(this.mesasHabilitadas));
-
-//        document.getElementById("mostrar").innerHTML = ("recaudacion", JSON.stringify(this.mesasHabilitadas) + "<br>" + "Total Recaudacion: $ " + this.totalRecaudacion);
-//     }
-
-//     ordenarArrayPorNumMesa(orden) {
-//         let arrayOrdenado = [];
-//         if (orden == 1) {
-//             arrayOrdenado = [...this.mesasHabilitadas.sort((a, b) => a.id - b.id)]
-//         } else { 
-//             arrayOrdenado = [...this.mesasHabilitadas.sort((a, b) => b.id - a.id)]
-//         }
-//         return arrayOrdenado;
-//     }
-
-//     ordenarArrayPorMonto(orden) {
-//         let arrayOrdenado2 = [];
-//         if (orden == 1) {
-//             arrayOrdenado2 = [...this.mesasHabilitadas.sort((a, b) => a.monto - b.monto)] 
-//         } else { 
-//             arrayOrdenado2 = [...this.mesasHabilitadas.sort((a, b) => b.monto - a.monto)]
-//         }
-//         return arrayOrdenado2;
-//     }
-// }
-
-// const morena = new LocalComercial();
-// morena.setearMesasHabilitadas();
-// console.log(morena.mesasHabilitadas); // muestro el arreglo
-// morena.recaudacionDia(); // ejecuto el metodo recaudacionDia
-
-// const arregloAsc = morena.ordenarArrayPorNumMesa(1);
-// console.log(arregloAsc) // muestro el arreglo ordenado ascendentemente 
-// const arregloDesc = morena.ordenarArrayPorNumMesa(2);
-// console.log(arregloDesc) // muestro el arreglo ordenado descendentemente
-
-// const arregloAscMonto = morena.ordenarArrayPorMonto(1);
-// console.log(arregloAscMonto) // muestro el arreglo ordenado ascendentemente 
-// const arregloDescMonto = morena.ordenarArrayPorMonto(2);
-// console.log(arregloDescMonto) // muestro el arreglo ordenado descendentemente
+/* Proyecto Avanzado: Voy a comenzar con la carga de los productos de la carta*/
 
 
-// /* Desafio 8 */
-
-// let texto = "";
-
-// const lista = ["getElementById()", "getElementClassName()", "getElementByTagName()"];
-
-// lista.forEach(mostrarLista);
-
-// document.getElementById("agregarArray").innerHTML = texto;
-
-// function mostrarLista(item, index) {
-//     texto += index + ": " + item + "<br>";
-// }
-
-/* DESAFIO 9 */
-// let cartaMenu = document.getElementById("cartaMenu");
-
-// cartaMenu.addEventListener('click', () => {
-//     console.log("Diste click")
-// });
-
-// class FormMensaje {
-//     constructor(nombre, apellido, mensaje){
-//         this.nombre = nombre;
-//         this.apellido = apellido;
-//         this.mensaje = mensaje;
-//     }
-// }
-
-// let mensajes = [];
-
-// let fomulario = document.getElementById("formulario");
-
-// formulario.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     let nombre = document.getElementById("nombre").value;
-//     let apellido = document.getElementById("apellido").value;
-//     let mensaje = document.getElementById("mensaje").value;
-
-//     const Mensaje = new FormMensaje(nombre, apellido, mensaje);
-
-//     mensajes.push(Mensaje);
-//     formulario.reset();
-//     document.getElementById("agregarArrayMensajes").innerHTML = JSON.stringify(mensajes);
-// });
-
-
-/* Proyecto Avanzado*/
-
-let cajaPrincipal = [];
-
-// const contenedorCaja = document.getElementById(contenedor-Caja);
-
-// const contenedorMesas = document.getElementById(contenedor-Mesas);
+//CASO EN QUE SEA YO LA QUE CARGUE TODOS LOS PRODUCTOS QUE EL CLIENTE VA A TENER EN LA PAGINA/ APLICACION
+// En este supuesto hice un array de productos/platos/menu y los cargue en forma manual
+// Luego muestro en la pagina, en el HTML, en forma de cards
 
 const contenedorMenu = document.getElementById("contenedor-Menu");
 
 mostrarMenu(stockMenu);
 
 function mostrarMenu(array) {
-    // console.log(array);
-    array.forEach(menu => {
-        let div = document.createElement("div");
+  // console.log(array);
+  array.forEach(menu => {
+    let div = document.createElement("div");
 
-        div.innerHTML += `<div class="card" style="width: 18rem;">
+    div.innerHTML += `<div class="card" style="width: 18rem; margin: 3rem;">
         <img src=${menu.img} style="width: 50px;height:50px;">
         <div class="card-body">
           <h5 class="card-title">${menu.nombre}</h5>
-          <p class="card-text">${menu.precio}</p>
-          <p class="card-text">${menu.desc}</p>
+          <p class="card-text">Precio: $${menu.precio}</p>
+          <p class="card-text">Descripcion: ${menu.desc}</p>
           <p class="card-text">Pedido Especial del cliente</p>
-          <p class="card-text">${menu.cantidad}</p>
-          <p class="card-text">SUBTOTAL</p>
+          <p class="card-text">stock: ${menu.stock}</p>
+          <p class="card-text">SUBTOTAL $ </p>
           <a id= "boton${menu.id}" href="#" class="btn btn-primary">Agregar</a>
         </div>
       </div>
@@ -174,7 +30,63 @@ function mostrarMenu(array) {
 
   </div>`
 
-  contenedorMenu.appendChild(div);
-});
-   
+    contenedorMenu.appendChild(div);
+  });
 }
+
+//SUPUESTO EN QUE SEA EL CLIENTE EL QUE CARGUE LOS PRODUCTOS
+// Es una buena opcion para no me llame cada vez que quiera agregar productos a la carta
+// Cree una clase producto
+/*NOTA PARA EL TUTOR: Lo que noto es que los productos no quedan cargados, cuando cierro
+la pagina y la vuelvo a abrir tengo que cargarlos nuevamente, esa no seria la idea. VER COMO
+PUEDO HACER*/
+
+class producto {
+  constructor(nombre, descripcion, precio, stock, imagen) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.stock = stock;
+    this.imagen = imagen;
+  }
+}
+
+//Array donde se van a acumular los productos que cargue el cliente
+let productos = [];
+
+//Boton que me permite mostrar los productos cargados en forma dinamica desde js
+let botonProductos = document.getElementById("botonProductos");
+
+//Donde el usuario de mi aplicacion va a ver los productos que cargo con el formulario
+let divProductos = document.getElementById("divProductos");
+
+//Formulario donde el cliente que ocupa mi aplicacion va a cargar las caracteristicas de los productos
+let formProductos = document.getElementById("formProductos");
+
+formProductos.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let datForm = new FormData(e.target);
+  let nuevoProd = new producto(datForm.get("nombre"), datForm.get("descripcion"), datForm.get("precio"), datForm.get("stock"), datForm.get("imagen"));
+  productos.push(nuevoProd);
+  localStorage.setItem('keyProductos', JSON.stringify(productos));
+  formProductos.reset()
+})
+
+botonProductos.addEventListener('click', () => {
+  let productosEnStorage = JSON.parse(localStorage.getItem('keyProductos'))
+  productosEnStorage.forEach((productosEnArray, indice) => {
+    divProductos.innerHTML += `<div class="card"; id= "producto ${indice}"; style="width: 18rem; margin: 3rem;">
+    
+    <div class="card-body">
+      <h5 class="card-title">${productosEnArray.nombre}</h5>
+      <p class="card-text">Precio: $${productosEnArray.precio}</p>
+      <p class="card-text">Descripcion: ${productosEnArray.descripcion}</p>
+      <p class="card-text">Pedido Especial del cliente</p>
+      <p class="card-text">Cantidad: ${productosEnArray.stock}</p>
+      <p class="card-text">SUBTOTAL $ </p>
+      <a id= "boton${indice}" href="#" class="btn btn-danger">Eliminar</a>
+    </div>
+
+</div>`
+  })
+})
--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 let stockMenu = [
-    {id: 1, nombre: "Hamburguesa Simple", tipo: "Hamburguesa", desc: "Pan, carne, queso", precio: 250, img: "./assets/hamburguesa-con-queso.png", cantidad:1},
-    {id: 2, nombre: "Hamburguesa Completa", tipo: "Hamburguesa", desc: "Pan, carne, queso, jamon, lechuga, tomate", precio: 300, img: "./assets/hamburguesa.png", cantidad:1},
-    {id: 3, nombre: "Hamburguesa Super", tipo: "Hamburguesa", desc: "Pan, carne, queso, jamon, lechuga, tomate, choclo", precio: 350, img: "./assets/hamburguesa.png", cantidad:1}
+    {id: 1, nombre: "Hamburguesa Simple", tipo: "Hamburguesa", desc: "Pan, carne, queso", precio: 250, img: "./assets/hamburguesa-con-queso.png", stock:1},
+    {id: 2, nombre: "Hamburguesa Completa", tipo: "Hamburguesa", desc: "Pan, carne, queso, jamon, lechuga, tomate", precio: 300, img: "./assets/hamburguesa.png", stock:1},
+    {id: 3, nombre: "Hamburguesa Super", tipo: "Hamburguesa", desc: "Pan, carne, queso, jamon, lechuga, tomate, choclo", precio: 350, img: "./assets/hamburguesa.png", stock:1}
 ]
